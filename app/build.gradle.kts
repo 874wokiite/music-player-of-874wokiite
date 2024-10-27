afdc28ba582752763f07a43309c187d1a1056a52
874wokiite <f.kanonon@gmail.com>
2024-10-27 23:27:30 +0900

    :construction: accompanist-navigation-animationを動くようにした

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.accompanist.navigation.animation)
-    implementation(libs.accompanist.shared.element)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
