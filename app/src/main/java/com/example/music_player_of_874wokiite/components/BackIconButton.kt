280e48105f658a5a306007be499e43f6b7971fb8
874wokiite <f.kanonon@gmail.com>
2024-11-03 20:34:47 +0900

    :sparkles: 音楽が再生停止されるようになった。前後の楽曲に切り替えることもできるようになった。

---
+++ app/src/main/java/com/example/music_player_of_874wokiite/components/BackIconButton.kt
@@ -0,0 +1,19 @@
+package com.example.music_player_of_874wokiite.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.SkipNext
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DownIconButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    IconButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowDown,
+            contentDescription = "SkipNext"
+        )
+    }
+}
--- app/src/main/java/com/example/music_player_of_874wokiite/features/musicDetail/MusicViewModel.kt
+++ app/src/main/java/com/example/music_player_of_874wokiite/features/musicDetail/MusicViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.music_player_of_874wokiite.features.musiclist.musicList
 import kotlinx.coroutines.launch
 
 class MusicViewModel(application: Application) : AndroidViewModel(application) {
@@ -15,6 +16,9 @@ class MusicViewModel(application: Application) : AndroidViewModel(application) {
     // MutableLiveDataで再生状態を管理
     private val _isPlaying = MutableLiveData(false)
     val isPlaying: LiveData<Boolean> = _isPlaying
+
+    private val trackList = musicList.map { it.audioFile } // 仮のトラックリスト
+    private var currentTrackIndex = 0
 
     fun prepareAndPlay(context: Context, audioFile: String) {
         viewModelScope.launch {
@@ -38,6 +42,18 @@ class MusicViewModel(application: Application) : AndroidViewModel(application) {
     fun resume() {
         mediaPlayer?.start()
         _isPlaying.value = true  // 再生状態を更新
+    }
+
+    fun nextTrack() {
+        currentTrackIndex = (currentTrackIndex + 1) % trackList.size
+        val nextMusic = musicList[currentTrackIndex]
+        prepareAndPlay(getApplication(), nextMusic.audioFile)
+    }
+
+    fun previousTrack() {
+        currentTrackIndex = if (currentTrackIndex - 1 < 0) trackList.size - 1 else currentTrackIndex - 1
+        val previousMusic = musicList[currentTrackIndex]
+        prepareAndPlay(getApplication(), previousMusic.audioFile)
     }
 
     override fun onCleared() {
