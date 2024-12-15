29bc4e757291cedf078623d67a367a897aa873da
874wokiite <f.kanonon@gmail.com>
2024-12-16 00:04:35 +0900

    :construction: スタイルをやや修正

--- app/src/main/java/com/example/music_player_of_874wokiite/components/MusicContent.kt
+++ app/src/main/java/com/example/music_player_of_874wokiite/components/MusicContent.kt
@@ -1,12 +1,15 @@
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,20 +35,22 @@ fun MusicContent(coverImage:String, musicTitle: String, albumTitle:String, modif
                 Image(
                     bitmap = it,
                     contentDescription = null,
-                    modifier = modifier.padding(bottom = 8.dp)
+                    modifier = modifier
+                        .padding(bottom = 8.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(8.dp)) // 枠線もつける場合
                 )
             }
         }
         Text(
             text = musicTitle,
             color = MaterialTheme.colorScheme.inverseSurface,
-            modifier = modifier,
+            modifier = modifier.padding(bottom = 4.dp),
             style = MaterialTheme.typography.bodyMedium
         )
         Text(
             text = albumTitle,
             color = MaterialTheme.colorScheme.inverseSurface,
-            modifier = modifier,
             style = MaterialTheme.typography.bodySmall
         )
     }
