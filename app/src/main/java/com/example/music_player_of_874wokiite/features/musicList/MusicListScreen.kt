dafeea9e7ced36c0ef881ab9fdc512264914e9b0
874wokiite <f.kanonon@gmail.com>
2024-12-15 22:05:54 +0900

    :feat: TopAppBarをリスト画面に追加

--- app/src/main/java/com/example/music_player_of_874wokiite/features/musicList/MusicListScreen.kt
+++ app/src/main/java/com/example/music_player_of_874wokiite/features/musicList/MusicListScreen.kt
@@ -1,14 +1,20 @@
 package com.example.music_player_of_874wokiite.features.musiclist
 
 import MusicContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.music_player_of_874wokiite.components.MyScaffold
+import com.example.music_player_of_874wokiite.ui.theme.MusicPlayerOf874wokiiteTheme
 
 
 // 音楽データを表すデータクラス
@@ -30,24 +36,48 @@ val musicList = listOf(
 
 // リスト表示画面
 @Composable
-fun MusicListScreen(navController: NavController, modifier: Modifier = Modifier, onRefreshPlay: (MusicData) -> Unit,) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        modifier = modifier
-    ) {
-        items(musicList.size) { index ->
-            val musicData = musicList[index]
-            MusicContent(
-                coverImage = musicData.coverImage,
-                musicTitle = musicData.musicTitle,
-                albumTitle = musicData.albumTitle,
-                modifier = Modifier
-                    .padding(8.dp)
-                    .clickable {
-                        // クリックで詳細画面へ遷移
-                        onRefreshPlay(musicData)
-                    }
-            )
+fun MusicListScreen(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onRefreshPlay: (MusicData) -> Unit,
+) {
+    MyScaffold(
+        title = "",
+        onProfileClick = { navController.navigate("profile") }
+    ) { innerPadding -> // Scaffoldの余白がここで渡される
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = modifier
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            items(musicList.size) { index ->
+                val musicData = musicList[index]
+                MusicContent(
+                    coverImage = musicData.coverImage,
+                    musicTitle = musicData.musicTitle,
+                    albumTitle = musicData.albumTitle,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .clickable {
+                            // クリックで詳細画面へ遷移
+                            onRefreshPlay(musicData)
+                        }
+                )
+            }
         }
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewMusicListScreen(modifier: Modifier = Modifier) {
+    MusicPlayerOf874wokiiteTheme {
+        MusicListScreen(
+            navController = NavController(LocalContext.current),
+            modifier = modifier,
+            onRefreshPlay = {}
+        )
     }
 }
