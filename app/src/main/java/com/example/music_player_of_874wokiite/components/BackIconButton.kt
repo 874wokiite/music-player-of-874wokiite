ca5bd8918bfae3218be3bc4873ffd8515059be0c
874wokiite <f.kanonon@gmail.com>
2025-01-25 23:54:20 +0900

    :sparkles:CloseButtonのデザインを反映

--- app/src/main/java/com/example/music_player_of_874wokiite/components/BackIconButton.kt
+++ app/src/main/java/com/example/music_player_of_874wokiite/components/BackIconButton.kt
@@ -1,18 +1,31 @@
 package com.example.music_player_of_874wokiite.components
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun DownIconButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
-    IconButton(onClick = onClick, modifier = modifier) {
+fun DownIconButton(onClick: () -> Unit) {
+    IconButton(
+        onClick = onClick,
+        modifier = Modifier.size(48.dp)
+    ) {
         Icon(
             imageVector = Icons.Filled.KeyboardArrowDown,
-            contentDescription = "SkipNext"
+            contentDescription = "SkipNext",
+            modifier = Modifier.size(32.dp)
         )
     }
+}
+
+@Preview
+@Composable
+fun PreviewDownIconButton() {
+    DownIconButton(onClick = {})
 }
