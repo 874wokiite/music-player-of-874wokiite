dc5278bdab62ab78d7b383cfe4d2a4ef97d1fe13
874wokiite <f.kanonon@gmail.com>
2025-07-27 14:44:33 +0900

    :art: .gitignoreにメディアファイルのパスを追加し、不要なアセットファイルを削除。MusicList.ktを更新して新しい楽曲データを追加。

--- app/src/main/java/com/example/music_player_of_874wokiite/model/MusicList.kt
+++ app/src/main/java/com/example/music_player_of_874wokiite/model/MusicList.kt
@@ -3,8 +3,10 @@ package com.example.music_player_of_874wokiite.model
 // リストデータ
 val musicList = listOf(
     MusicData("happyReborn.png", "Haribo", "Haribo Album", "happyReborn.mp3"),
-    MusicData("xxxDay.png", "xxxDay", "xxxDay", "happyReborn.mp3"),
     MusicData("rememberApathy.png", "思い出したアパシー", "思い出したアパシー", "rememberApathy.mp3"),
+    MusicData("xxxDay.png", "xxxDay", "xxxDay", "happyReborn.mp3"),
     MusicData("goGoGo.png", "はしろ", "xxxDay", "rememberApathy.mp3"),
-    MusicData("doku.png", "毒", "xxxDay", "happyReborn.mp3")
+    MusicData("doku.png", "毒", "xxxDay", "happyReborn.mp3"),
+    MusicData("antiGuzonizm.png", "antiGuzonizm", "antiGuzonizm Album", "antiGuzonizm.mp3"),
+    MusicData("refrain.png", "Refrain", "Refrain Album", "refrain.mp3")
 )
